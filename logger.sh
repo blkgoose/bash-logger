431bc6ce717ee6e4bee991ebc5272fd7a0946d77
blkgoose <alessio.biancone@gmail.com>
2019-01-10 11:09:11 +0100

    Added logger script

---
+++ logger.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+logfile="$0.log"
+loglevel="INFO"
+levels="DEBUG INFO WARN ERROR"
+
+log() {
+    timestamp=$(date +"%D %T")
+    level="$1"
+    message="$2"
+    out="$timestamp|$level|$message"
+
+    echo -e "$level: $message"
+    echo -e "$out" >> "$logfile"
+}
+
+is_valid() {
+    [[ $(echo $levels | grep -oP "$loglevel.*" ) =~ "$1" ]]
+}
+
+debug() {
+    is_valid "DEBUG" &&
+        log "DEBUG" "$1"
+}
+info() {
+    is_valid "INFO" &&
+        log "INFO" "$1"
+}
+warn() {
+    is_valid "WARN" &&
+        log "WARN" "$1"  >&2
+}
+error() {
+    is_valid "ERROR" && {
+        log "ERROR" "$1" >&2
+        exit
+    }
+}
